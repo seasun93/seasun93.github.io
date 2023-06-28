17f8bb85e5d773bac587991faacd518bf50526fb
seasun93 <seasun9376@gmail.com>
2023-06-28 14:32:29 +0900

    nav js 추가

---
+++ _site/assets/js/sidebar-folder.js
@@ -0,0 +1,32 @@
+const depsHeight = [];
+
+window.addEventListener('load',depsList)
+
+function depsList(){
+    const deps = document.querySelectorAll('.deps');
+    const depsTitle = document.querySelectorAll('.deps-title');
+
+    deps.forEach(function(el){
+        depsHeight.push(el.scrollHeight)
+    })
+    depsTitle.forEach(function(el,i){
+        el.addEventListener('click',function(){
+            if(this.classList.contains('on')){
+                removeAll()
+            } else {
+                deps[i].classList.remove('on');
+                deps[i].style.height = `${depsHeight[i]}px`;
+                depsTitle[i].classList.remove('on');
+            }
+        })
+    })
+
+    function removeAll(){
+        for(let i = 0; i < deps.length; i++){
+            deps[i].classList.remove('on');
+            deps[i].style.height = `0px`;
+            depsTitle[i].classList.remove('on');
+        }
+    }
+    
+}
